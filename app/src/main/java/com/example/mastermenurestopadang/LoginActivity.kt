3453084c72826a466d0a129b3afde235da512750
ste28 <stefanus2800@gmail.com>
2022-06-03 14:25:29 +0700

    masih error dalam aplikasi tidak bisa di start

--- app/src/main/java/com/example/mastermenurestopadang/LoginActivity.kt
+++ app/src/main/java/com/example/mastermenurestopadang/LoginActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mastermenurestopadang
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -16,10 +15,26 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        loginFragment = supportFragmentManager.findFragmentById(R.id.login_frag) as LoginFragment
+        loginFragment.onLoginListener = {username, password ->
+            if (username == "admin" && password == "admin") {
+                Toast.makeText(this, "Login admin berhasil!", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                var u = users.find {
+                    return@find it.username == username && it.password == password
+                }
 
-
-
-
+                if (u != null) {
+//                    val intent = Intent(this, MainActivity2::class.java)
+//                    intent.putExtra("user", u)
+//                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this, "Login gagal!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -30,49 +45,9 @@ class LoginActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.login_menu) {
             val fragment = LoginFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.login_regist_frag, fragment).setReorderingAllowed(true).commit()
-            loginFragment = supportFragmentManager.findFragmentById(R.id.login_regist_frag) as LoginFragment
-            loginFragment.onLoginListener = {username, password ->
-                if (username == "admin" && password == "admin") {
-                    Toast.makeText(this, "Login admin berhasil!", Toast.LENGTH_SHORT).show()
-                }
-                else {
-                    var u = users.find {
-                        return@find it.username == username && it.password == password
-                    }
-
-                    if (u != null) {
-//                    val intent = Intent(this, MainActivity2::class.java)
-//                    intent.putExtra("user", u)
-//                    startActivity(intent)
-                    }
-                    else {
-                        Toast.makeText(this, "Login gagal!", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
+            supportFragmentManager.beginTransaction().replace(R.id.login_frag, fragment).setReorderingAllowed(true).commit()
         } else if (item.itemId == R.id.register_menu) {
-            val fragment = RegisterFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.login_regist_frag, fragment).setReorderingAllowed(true).commit()
-            registerFragment = supportFragmentManager.findFragmentById(R.id.login_regist_frag) as RegisterFragment
-            registerFragment.onRegisterListener = { username, nama, alamat, password ->
-                var u = users.find {
-                    return@find it.username == username
-                }
 
-                if (u != null) {
-                    Toast.makeText(this, "Username sudah pernah digunakan!", Toast.LENGTH_SHORT).show()
-                } else {
-                    val u = User(username, nama, alamat, password)
-                    users.add(u)
-                    val fragment = UserFragment()
-                    val bundle = Bundle()
-                    bundle.putParcelable("user", u)
-
-                    fragment.arguments = bundle
-                    Toast.makeText(this, "Register Berhasil", Toast.LENGTH_SHORT).show()
-                }
-            }
         }
         return true
     }
