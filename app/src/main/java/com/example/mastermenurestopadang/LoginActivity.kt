0bea2cc06c07689351191ac9c9267e91b8385687
ste28 <stefanus2800@gmail.com>
2022-06-09 18:53:25 +0700

    sudah bisa pindah register to login

--- app/src/main/java/com/example/mastermenurestopadang/LoginActivity.kt
+++ app/src/main/java/com/example/mastermenurestopadang/LoginActivity.kt
@@ -1,19 +1,27 @@
 package com.example.mastermenurestopadang
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class LoginActivity : AppCompatActivity() {
-    val users:ArrayList<User> = ArrayList()
     lateinit var loginFragment: LoginFragment
-    lateinit var registerFragment: RegisterFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var users:ArrayList<User> = ArrayList()
+        val intent = getIntent()
+        val user = intent.getParcelableExtra<User>("newUser")
+        if (user != null) {
+            users.add(user)
+        }
         
         loginFragment = supportFragmentManager.findFragmentById(R.id.login_frag) as LoginFragment
         loginFragment.onLoginListener = {username, password ->
@@ -29,6 +37,10 @@ class LoginActivity : AppCompatActivity() {
 //                    val intent = Intent(this, MainActivity2::class.java)
 //                    intent.putExtra("user", u)
 //                    startActivity(intent)
+                    for (i in 0 until users.count()){
+                        Toast.makeText(this, users[i].username, Toast.LENGTH_SHORT).show()
+                    }
+                    Toast.makeText(this, "Login Sukses bro", Toast.LENGTH_SHORT).show()
                 }
                 else {
                     Toast.makeText(this, "Login gagal!", Toast.LENGTH_SHORT).show()
@@ -44,10 +56,11 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.login_menu) {
-            val fragment = LoginFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.login_frag, fragment).setReorderingAllowed(true).commit()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         } else if (item.itemId == R.id.register_menu) {
-
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
         return true
     }
---
+++ app/src/main/java/com/example/mastermenurestopadang/RegisterActivity.kt
@@ -0,0 +1,53 @@
+package com.example.mastermenurestopadang
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class RegisterActivity : AppCompatActivity() {
+    val users:ArrayList<User> = ArrayList()
+    lateinit var registerFragment: RegisterFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        registerFragment = supportFragmentManager.findFragmentById(R.id.regist_frag) as RegisterFragment
+        registerFragment.onRegisterListener = {username, nama, alamat, password ->
+            var u = users.find {
+                return@find it.username == username
+            }
+
+            if (u != null) {
+                Toast.makeText(this, "Username sudah pernah digunakan!", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val u = User(username, nama, alamat, password)
+                users.add(u)
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.putExtra("newUser", users)
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu_login_register, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.login_menu) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        } else if (item.itemId == R.id.register_menu) {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        return true
+    }
+}
